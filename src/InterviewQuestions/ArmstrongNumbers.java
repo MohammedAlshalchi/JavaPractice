e87be89e76538c54d2f2252cff53fd419225239c
Mohammed <95448569+MohammedAlshalchi@users.noreply.github.com>
2022-03-01 22:40:21 -0500

    all done

--- src/InterviewQuestions/ArmstrongNumbers.java
+++ src/InterviewQuestions/ArmstrongNumbers.java
@@ -1,9 +1,49 @@
 package InterviewQuestions;
 
+import java.util.Scanner;
+
 public class ArmstrongNumbers {
     /*
     2. Numbers -- Armstrong numbers
 Write a method that can check if a number is
 Armstrong number
      */
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input number");
+        int num = input.nextInt();
+
+
+       boolean armstrongNumber = ArmStrongNumber(num);
+       if (armstrongNumber){
+           System.out.println(armstrongNumber+" : "+" is an Armstrong number ");
+       }else {
+           System.out.println(armstrongNumber+" : "+" is not an Armstrong number ");
+       }
+
+    }
+
+    public  static  boolean ArmStrongNumber (int  num) {
+
+        int a = 0,    b =0,    c= num;
+
+        while(num>0){
+
+            a = num% 10;
+            num /= 10;
+            b= b + (a*a*a);
+        }
+
+        if(c == b) {
+            return true;
+        }
+        return false;
+    }
+
+
+
+
+
 }
--- src/InterviewQuestions/ReverseNegativeNumber.java
+++ src/InterviewQuestions/ReverseNegativeNumber.java
@@ -1,18 +1,7 @@
 package InterviewQuestions;
 
-import java.util.Arrays;
 
-public class ReverseNegativeNumber {
-    /*
-    3. Number -- Reverse negative number
-Write a return method that can reverse
-negative number and return it as int
-     */
-
-
-
-
-    // solution1
+// solution1
 //    create a method that return the reversed array
 //
 //    String sum= reversedArray(new int[] { 2,7,3,9});
@@ -49,19 +38,61 @@ negative number and return it as int
 for (int i = array.length - 1, j=0 ; i >= 0; i--,j++) {
       reversedArray[j] = array[i];
 
-
-
    }
         System.out.println(Arrays.toString(reversedArray));
 }
 
     */
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReverseNegativeNumber {
+    /*
+    3. Number -- Reverse negative number
+Write a return method that can reverse
+negative number and return it as int
+     */
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input number");
+        int number = input.nextInt();
+
+        if (number >= 0){
+            System.out.println("Invalid number");
+            System.exit(0);
+        }
+        int newNumber = reverseNumber(number);
+        System.out.println(newNumber);
+
+    }
+
+    public static int reverseNumber(int number) {
+
+            int number2 = Math.abs(number);
+            String number3 = "" + number2;
+            String[] arrayToNumber = number3.split("");
+            String[] reversedNumber = new String[arrayToNumber.length];
+            for (int i = arrayToNumber.length - 1, j = 0; i >= 0; i--, j++) {
+                reversedNumber[j] =arrayToNumber[i];
+            }
+            String temporary  = "";
+            for (int r = 0; r < reversedNumber.length; r++) {
+
+               temporary += reversedNumber[r];
+            }
+
+        return Integer.parseInt(temporary);
+    }
 
 
 
 
-   }
 
+
+
+}
 
 
