4129b29667eb89c56182e3deaecea453e08e31af
Mohammed <95448569+MohammedAlshalchi@users.noreply.github.com>
2022-03-15 19:16:30 -0400

    5th interview question

---
+++ src/InterviewQuestions/StringFindTheUnique.java
@@ -0,0 +1,53 @@
+package InterviewQuestions;
+
+public class StringFindTheUnique {
+    /*
+   String -- Find the unique
+Write a return method that can find the unique characters
+from the String
+Ex: unique("AAABBBCCCDEF") ==> "DEF"
+     */
+
+    public static void main(String[] args) {
+        String str = "AAABBBCCCDEF";
+
+      //String uniqueCharactor   = uniqueCharactors(str);
+        System.out.println(uniqueCharactors(str));
+    }
+
+
+
+
+
+    public static String uniqueCharactors (String str){
+
+        String[] arr = str.split("");
+        String unique = "";
+
+
+        for (int i = 0; i < arr.length; i++) {
+            int frequency = 0;
+
+            for (int i1 = 0; i1 < arr.length; i1++) {
+                if (arr[i1].equals(arr[i])){
+                    frequency++;
+                }
+            }
+            if (frequency == 1){
+                unique += arr[i];
+            }
+        }
+        
+        
+        
+        return unique;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ src/InterviewQuestions/StringReverse.java
@@ -0,0 +1,9 @@
+package InterviewQuestions;
+
+public class StringReverse {
+    /*
+    String -- Reverse
+Write a return method that can reverse String
+Ex: Reverse("ABCD"); ==> DCBA
+     */
+}
