b65a5099c281b48e8dc70cb4a586c56d40a2aa30
Mohammed <95448569+MohammedAlshalchi@users.noreply.github.com>
2022-03-21 12:13:07 -0400

    6th interview question

---
+++ src/InterviewQuestions/ArrayFindMaximum.java
@@ -0,0 +1,7 @@
+package InterviewQuestions;
+
+public class ArrayFindMaximum {
+    /*
+    Write a method that can find the maximum number from an int Array
+     */
+}
---
+++ src/InterviewQuestions/SumOfDigitsInA_String.java
@@ -0,0 +1,8 @@
+package InterviewQuestions;
+
+public class SumOfDigitsInA_String {
+    /*
+    Write a method that can return the sum of the digits in a string
+     */
+
+}
